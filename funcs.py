62a0235efb0c8a1fcea319f1b46497ea5632fd34
karl-gardner <karl.gardner@ttu.edu>
2022-10-12 18:47:38 -0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -29,34 +29,24 @@ def compile_data(data, sample="s12345"):
     temp = np.concatenate((s1_t1[:,0],s1_t2[:,0],s1_t3[:,0],s2_t1[:,0],s2_t2[:,0],s2_t3[:,0],s3_t1[:,0], s3_t2[:,0], s3_t3[:,0],s4_t1[:,0], s4_t2[:,0]))
     hum = np.concatenate((s1_t1[:,1],s1_t2[:,1],s1_t3[:,1],s2_t1[:,1],s2_t2[:,1],s2_t3[:,1],s3_t1[:,1], s3_t2[:,1], s3_t3[:,1],s4_t1[:,1], s4_t2[:,1]))
     thick = np.concatenate((s1_t1[:,2],s1_t2[:,2],s1_t3[:,2],s2_t1[:,2],s2_t2[:,2],s2_t3[:,2],s3_t1[:,2], s3_t2[:,2], s3_t3[:,2],s4_t1[:,2], s4_t2[:,2]))
-    s1_end = s1_t1.shape[0]+s1_t2.shape[0]+s1_t3.shape[0]
-    s2_end = s1_end+s2_t1.shape[0]+s2_t2.shape[0]+s2_t3.shape[0]
-    s3_end = s2_end+s3_t1.shape[0]+s3_t2.shape[0]+s3_t3.shape[0]
-    s4_end = s3_end+s4_t1.shape[0]+s4_t2.shape[0]
     
   if sample=="s123":
     # Samples 1,2,3
     temp = np.concatenate((s1_t1[:,0],s1_t2[:,0],s1_t3[:,0],s2_t1[:,0],s2_t2[:,0],s2_t3[:,0],s3_t1[:,0], s3_t2[:,0], s3_t3[:,0]))
     hum = np.concatenate((s1_t1[:,1],s1_t2[:,1],s1_t3[:,1],s2_t1[:,1],s2_t2[:,1],s2_t3[:,1],s3_t1[:,1], s3_t2[:,1], s3_t3[:,1]))
     thick = np.concatenate((s1_t1[:,2],s1_t2[:,2],s1_t3[:,2],s2_t1[:,2],s2_t2[:,2],s2_t3[:,2],s3_t1[:,2], s3_t2[:,2], s3_t3[:,2]))
-    s1_end = s1_t1.shape[0]+s1_t2.shape[0]+s1_t3.shape[0]
-    s2_end = s1_end+s2_t1.shape[0]+s2_t2.shape[0]+s2_t3.shape[0]
-    s3_end = s2_end+s3_t1.shape[0]+s3_t2.shape[0]+s3_t3.shape[0]
     
   if sample=="s4":
     # Sample 4
     temp = np.concatenate((s4_t1[:,0], s4_t2[:,0]))
     hum = np.concatenate((s4_t1[:,1], s4_t2[:,1]))
     thick = np.concatenate((s4_t1[:,2], s4_t2[:,2]))
-    s3_end = 0
-    s4_end = s3_end+s4_t1.shape[0]+s4_t2.shape[0]
     
   if sample=="s5":
     # Sample 5
     temp = np.concatenate((s5_t1[:,0], s5_t2[:,0], s5_t3[:,0]))
     hum = np.concatenate((s5_t1[:,1], s5_t2[:,1], s5_t3[:,1]))
     thick = np.concatenate((s5_t1[:,2], s5_t2[:,2], s5_t3[:,2]))
-    s4_end = 0
   
   s1_end = s1_t1.shape[0]+s1_t2.shape[0]+s1_t3.shape[0]
   s2_end = s1_end+s2_t1.shape[0]+s2_t2.shape[0]+s2_t3.shape[0]
@@ -67,4 +57,47 @@ def compile_data(data, sample="s12345"):
     
   return temp, hum, thick, indices
 
+
+
+def make_graph(X,Y,Z,temp,hum,thick,indices,sample="s1234"):
+  s1_end,s2_end,s3_end,s4_end,s5_end = indices
+  if sample=="s12345":
+    fig = go.Figure(data=[
+    go.Surface(x=X, y=Y, z=Z, opacity=0.7, colorscale="YlOrRd",colorbar = {"len":0.75, "thickness": 70, "x": 0.88, "y":0.4, "tickfont":{"size":40}}),
+    go.Scatter3d(x=temp[:s1_end], y=hum[:s1_end], z=thick[:s1_end], mode="markers", marker={"symbol": "cross","color":"black", "size": 17}),
+    go.Scatter3d(x=temp[s1_end:s2_end], y=hum[s1_end:s2_end], z=thick[s1_end:s2_end], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+    go.Scatter3d(x=temp[s2_end:s3_end], y=hum[s2_end:s3_end], z=thick[s2_end:s3_end], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+    go.Scatter3d(x=temp[s3_end:s4_end], y=hum[s3_end:s4_end], z=thick[s3_end:s4_end], mode="markers", marker={"symbol": "circle-open", "color":"black", "size": 17}),
+    go.Scatter3d(x=temp[s4_end:], y=hum[s4_end:], z=thick[s4_end:], mode="markers", marker={"symbol": "circle", "color":"black", "size": 14})
+    ])
     
+  if sample=="s1234":
+    fig = go.Figure(data=[
+      go.Surface(x=X, y=Y, z=Z, opacity=0.7, colorscale="YlOrRd",colorbar = {"len":0.75, "thickness": 70, "x": 0.88, "y":0.4, "tickfont":{"size":40}}),
+      go.Scatter3d(x=temp[:s1_end], y=hum[:s1_end], z=thick[:s1_end], mode="markers", marker={"symbol": "cross","color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s1_end:s2_end], y=hum[s1_end:s2_end], z=thick[s1_end:s2_end], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s2_end:s3_end], y=hum[s2_end:s3_end], z=thick[s2_end:s3_end], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s3_end:s4_end], y=hum[s3_end:s4_end], z=thick[s3_end:s4_end], mode="markers", marker={"symbol": "circle-open", "color":"black", "size": 17})
+    ])
+  if sample="s123":
+    fig = go.Figure(data=[
+      go.Surface(x=X, y=Y, z=Z, opacity=0.7, colorscale="YlOrRd",colorbar = {"len":0.75, "thickness": 70, "x": 0.88, "y":0.4, "tickfont":{"size":40}}),
+      go.Scatter3d(x=temp[:s1_end], y=hum[:s1_end], z=thick[:s1_end], mode="markers", marker={"symbol": "cross","color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s1_end:s2_end], y=hum[s1_end:s2_end], z=thick[s1_end:s2_end], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s2_end:s3_end], y=hum[s2_end:s3_end], z=thick[s2_end:s3_end], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17})
+    ])
+  if sample="s4":
+    fig = go.Figure(data=[
+      go.Surface(x=X, y=Y, z=Z, opacity=0.7, colorscale="YlOrRd",colorbar = {"len":0.75, "thickness": 70, "x": 0.88, "y":0.4, "tickfont":{"size":40}}),
+      go.Scatter3d(x=temp[0:temp.shape[0]], y=hum[0:temp.shape[0]], z=thick[0:temp.shape[0]], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17})
+    ])
+  if sample="s5":
+    fig = go.Figure(data=[
+      go.Surface(x=X, y=Y, z=Z, opacity=0.7, colorscale="YlOrRd",colorbar = {"len":0.75, "thickness": 70, "x": 0.88, "y":0.4, "tickfont":{"size":40}}),
+      go.Scatter3d(x=temp[0:temp.shape[0]], y=hum[0:temp.shape[0]], z=thick[0:temp.shape[0]], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17})
+    ])
+  return fig
+
+  
+  
+  
