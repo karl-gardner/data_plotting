313861601b7773bd4f5a0a3ef648c0c629c0eb89
karl-gardner <karl.gardner@ttu.edu>
2022-10-22 17:06:23 -0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
-def compile_data(data, sample="s12345"):
+def compile_data(data, sample="s1234"):
   # preprocess dataframe
   s1_t1 = data[["Sample 1 (320nm)", "Unnamed: 1", "Unnamed: 2"]].to_numpy().astype(float)
   s1_t2 = data[["Sample 1 (320nm)", "Unnamed: 1", "Unnamed: 3"]].to_numpy().astype(float)
@@ -36,7 +36,12 @@ def compile_data(data, sample="s12345"):
     temp = np.concatenate((s1_t1[:,0],s1_t2[:,0],s1_t3[:,0],s2_t1[:,0],s2_t2[:,0],s2_t3[:,0],s3_t1[:,0], s3_t2[:,0], s3_t3[:,0]))
     hum = np.concatenate((s1_t1[:,1],s1_t2[:,1],s1_t3[:,1],s2_t1[:,1],s2_t2[:,1],s2_t3[:,1],s3_t1[:,1], s3_t2[:,1], s3_t3[:,1]))
     thick = np.concatenate((s1_t1[:,2],s1_t2[:,2],s1_t3[:,2],s2_t1[:,2],s2_t2[:,2],s2_t3[:,2],s3_t1[:,2], s3_t2[:,2], s3_t3[:,2]))
-    
+  
+  if sample=="s1235":
+    temp = np.concatenate((s1_t1[:,0],s1_t2[:,0],s1_t3[:,0],s2_t1[:,0],s2_t2[:,0],s2_t3[:,0],s3_t1[:,0], s3_t2[:,0], s3_t3[:,0], s5_t1[:,0], s5_t2[:,0], s5_t3[:,0]))
+    hum = np.concatenate((s1_t1[:,1],s1_t2[:,1],s1_t3[:,1],s2_t1[:,1],s2_t2[:,1],s2_t3[:,1],s3_t1[:,1], s3_t2[:,1], s3_t3[:,1], s5_t1[:,1], s5_t2[:,1], s5_t3[:,1]))
+    thick = np.concatenate((s1_t1[:,2],s1_t2[:,2],s1_t3[:,2],s2_t1[:,2],s2_t2[:,2],s2_t3[:,2],s3_t1[:,2], s3_t2[:,2], s3_t3[:,2], s5_t1[:,2], s5_t2[:,2], s5_t3[:,2]))
+  
   if sample=="s4":
     # Sample 4
     temp = np.concatenate((s4_t1[:,0], s4_t2[:,0]))
@@ -90,6 +95,14 @@ def make_graph(X,Y,Z,temp,hum,thick,sizes,sample="s1234"):
       go.Scatter3d(x=temp[:s1], y=hum[:s1], z=thick[:s1], mode="markers", marker={"symbol": "cross","color":"black", "size": 17}),
       go.Scatter3d(x=temp[s1:s12], y=hum[s1:s12], z=thick[s1:s12], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
       go.Scatter3d(x=temp[s12:], y=hum[s12:], z=thick[s12:], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+    ])
+    if sample=="s1235":
+    fig = go.Figure(data=[
+      go.Surface(x=X, y=Y, z=Z, opacity=0.7, colorscale="YlOrRd",colorbar = {"len":0.75, "thickness": 70, "x": 0.88, "y":0.4, "tickfont":{"size":40}}),
+      go.Scatter3d(x=temp[:s1], y=hum[:s1], z=thick[:s1], mode="markers", marker={"symbol": "cross","color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s1:s12], y=hum[s1:s12], z=thick[s1:s12], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s12:s123], y=hum[s12:s123], z=thick[s12:s123], mode="markers", marker={"symbol": "cross", "color":"black", "size": 17}),
+      go.Scatter3d(x=temp[s1234:], y=hum[s1234:], z=thick[s1234:], mode="markers", marker={"symbol": "diamond-open", "color":"black", "size": 14})
     ])
   if sample=="s4":
     fig = go.Figure(data=[
